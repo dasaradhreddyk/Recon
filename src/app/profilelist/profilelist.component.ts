4a977a6fa8b6fdb7b2e3f0bba608889143c263b6
dasaradhreddyk <58421595+dasaradhreddyk@users.noreply.github.com>
2022-09-10 06:23:41 +1000

    Update profilelist.component.ts

--- src/app/profilelist/profilelist.component.ts
+++ src/app/profilelist/profilelist.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders, HttpResponse } from '@angular/common/http';
+
+import { map, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
 @Component({
   selector: 'app-profilelist',
   templateUrl: './profilelist.component.html',
@@ -13,7 +17,8 @@ export class ProfilelistComponent implements OnInit {
   posts : ProfileData[] =[];
   public CHARACTERS: any[]=[];
   public Details:string ="";
-   
+  public tag:string="";
+  public username:string="";
   constructor(private http: HttpClient) { }
    
   ngOnInit(): void {
@@ -37,6 +42,7 @@ export class ProfilelistComponent implements OnInit {
     this.Details =value;
     //HTTPS://WWW.FACEBOOK.COM/KIM.XYZ
     this.Details = value.split('/')[3];
+    this.username = this.Details;
     this.Details = this.Details.split('.')[0];
     var param = this.Details.replace(/[0-9]/g, '');
     // console.log("*********"+'https://api.genderize.io/?name='+value.split('.')[0]);
@@ -47,6 +53,36 @@ export class ProfilelistComponent implements OnInit {
        })
 
   }
+  getSelectedValue(event:any){
+  
+    // Prints selected value
+    this.tag = event.target.value;
+    console.log(event.target.value);
+  }
+  BookMarkUser()
+  {
+    let httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache'
+  });
+
+    this.http.post<any>
+    ('http://localhost:8840/api/BookMark/AddBookMark', 
+    { user: this.username,tag:this.tag }, { headers: httpHeaders }).subscribe(data => {
+      console.log("sucess");
+    })
+    
+    console.log("inside bookmark");
+    
+  }
+  private handleErrorObservable(error: any) {
+    console.error(error.message || error);
+    return throwError(error);
+  } 
+  private extractData(res: any) {
+    let body = res;
+    return body;
+}
   getProfileData(value: string)
   {
 	  console.log("Click Event" + value);
